a6913652d370118e8c9183c8bdf1617167fba041
hyouka <2100012746@stu.pku.edu.cn>
2023-09-19 21:44:23 +0800

    add sqrt.c/h

---
+++ src/common/sqrt.c
@@ -0,0 +1,4 @@
+#include "common/sqrt.h"
+#include <stdint.h>
+#include <stdio.h>
+
---
+++ src/common/sqrt.h
@@ -0,0 +1,14 @@
+#ifndef SQRT_H
+#define SQRT_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    //void softmax(int*, int*, int, int);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
